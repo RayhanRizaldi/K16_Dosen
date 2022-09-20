f5ad6cd3ce98bcd7db57ddb608570dffd774074b
winanda afrilia harisya <winandaafrilia0304@gmail.com>
2022-09-20 12:35:06 +0700

    bagian winanda(1)

--- app/src/main/java/com/tbptb_b/k16_dosen/JSeminarActivity.java
+++ app/src/main/java/com/tbptb_b/k16_dosen/JSeminarActivity.java
@@ -12,10 +12,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class JSeminarActivity extends AppCompatActivity {
-    RecyclerView recyclerView;
-    LinearLayoutManager linearLayoutManager;
-    AdaptorDataRV adapterDataRV;
-    List<String> listData;
 
 
     @Override
@@ -23,18 +19,5 @@ public class JSeminarActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jseminar);
 
-        recyclerView = findViewById(R.id.rvData);
-        listData = new ArrayList<>();
-
-        for (int i=0; i<10; i++){
-            listData.add("data Ke "+i);
-
-        }
-        linearLayoutManager= new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
-        recyclerView.setLayoutManager(linearLayoutManager);
-
-        adapterDataRV = new AdaptorDataRV(this, listData);
-        recyclerView.setAdapter(adapterDataRV);
-        adapterDataRV.notifyDataSetChanged();
     }
 }
