2117efbeea50d9b4d480e70fb3f7486168dd7970
RayhanRizaldi <2011522021@student.unand.ac.id>
2022-09-20 11:28:00 +0700

    login&themes

--- app/src/main/java/com/tbptb_b/k16_dosen/LoginActivity.java
+++ app/src/main/java/com/tbptb_b/k16_dosen/LoginActivity.java
@@ -2,13 +2,29 @@ package com.tbptb_b.k16_dosen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        button = findViewById(R.id.button_login);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMainMenu();
+            }
+        });
+    }
+
+    public void openMainMenu(){
+        Intent LoginIntent = new Intent(this, MainMenu.class);
+        startActivity(LoginIntent);
     }
 }
