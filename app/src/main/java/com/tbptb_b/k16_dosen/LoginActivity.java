ea1aa50c179273e391bd88fa7ec833af6a874279
RayhanRizaldi <2011522021@student.unand.ac.id>
2022-09-20 15:56:12 +0700

    gin

--- app/src/main/java/com/tbptb_b/k16_dosen/LoginActivity.java
+++ app/src/main/java/com/tbptb_b/k16_dosen/LoginActivity.java
@@ -6,9 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
     private Button button;
+    EditText editUsername,editPassword;
+    String username,password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +20,21 @@ public class LoginActivity extends AppCompatActivity {
 
         button = findViewById(R.id.button_login);
         button.setOnClickListener(view -> openMainMenu());
+
+//        editUsername = (EditText) findViewById(R.id.editUsername);
+//        username = editUsername.getEditableText().toString();
+//
+//        editPassword = (EditText) findViewById(R.id.editPassword);
+//        username = editPassword.getEditableText().toString();
+//
+//        if(password.equals("yeyjadi")){
+//            Intent LoginIntent = new Intent(this, MainMenu.class);
+//            LoginIntent.putExtra("username",username);
+//            LoginIntent.putExtra("cek_login",true);
+//            startActivity(LoginIntent);
+//        }else{
+//            Toast.makeText(this,"Yah Gagal",Toast.LENGTH_SHORT).show();
+//        }
     }
 
     public void openMainMenu(){
