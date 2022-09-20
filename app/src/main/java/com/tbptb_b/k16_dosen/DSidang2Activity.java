5aa5652a923487c03d31d688408c5803147065a9
winanda afrilia harisya <winandaafrilia0304@gmail.com>
2022-09-20 12:33:48 +0700

    bagian winanda(1)

--- app/src/main/java/com/tbptb_b/k16_dosen/DSidang2Activity.java
+++ app/src/main/java/com/tbptb_b/k16_dosen/DSidang2Activity.java
@@ -10,5 +10,6 @@ public class DSidang2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dsidang2);
+
     }
 }
