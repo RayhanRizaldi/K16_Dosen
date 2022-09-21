705f6766020fbeaa89ee3a9645ba1553d18a76d7
winanda afrilia harisya <winandaafrilia0304@gmail.com>
2022-09-21 18:24:17 +0700

    layout done (tinggal ricycle view )

--- app/src/main/java/com/tbptb_b/k16_dosen/JseminarActivity.java
+++ app/src/main/java/com/tbptb_b/k16_dosen/JseminarActivity.java
@@ -1,23 +1,14 @@
 package com.tbptb_b.k16_dosen;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.LinearLayout;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class JSeminarActivity extends AppCompatActivity {
-
+public class JseminarActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jseminar);
-
     }
 }
