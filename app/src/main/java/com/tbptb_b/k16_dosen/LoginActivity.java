f1efdcae8965422d12654f337081f50502f5302c
RayhanRizaldi <2011522021@student.unand.ac.id>
2022-09-21 14:56:25 +0700

    valid

--- app/src/main/java/com/tbptb_b/k16_dosen/LoginActivity.java
+++ app/src/main/java/com/tbptb_b/k16_dosen/LoginActivity.java
@@ -4,27 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
-    private Button button;
+    Button btnLogin;
+    TextView btn;
     EditText editUsername,editPassword;
-    String username,password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        button = findViewById(R.id.button_login);
-        button.setOnClickListener(view -> openMainMenu());
+        setupUI();
+        setupListeners();
 
-//        editUsername = (EditText) findViewById(R.id.editUsername);
+
+//        btnLogin = findViewById(R.id.button_login);
+//        btnLogin.setOnClickListener(view -> cekCrededentials());
+
 //        username = editUsername.getEditableText().toString();
 //
-//        editPassword = (EditText) findViewById(R.id.editPassword);
+
 //        username = editPassword.getEditableText().toString();
 //
 //        if(password.equals("yeyjadi")){
@@ -37,9 +42,53 @@ public class LoginActivity extends AppCompatActivity {
 //        }
     }
 
-    public void openMainMenu(){
-        Intent LoginIntent = new Intent(this, MainMenu.class);
-        startActivity(LoginIntent);
-        finish();
+    private void setupUI() {
+        editUsername = (EditText) findViewById(R.id.editUsername);
+        editPassword = (EditText) findViewById(R.id.editPassword);
+        btnLogin = findViewById(R.id.button_login);
     }
+
+    private void setupListeners() {
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkUsername();
+            }
+        });
+
+    }
+
+    void checkUsername(){
+        boolean valid=true;
+        if(valid){
+            String username = editUsername.getEditableText().toString();
+            String password = editPassword.getEditableText().toString();
+            if(password.equals("yeyjadi")){
+                Intent LoginIntent = new Intent(this, MainMenu.class);
+                LoginIntent.putExtra("username",username);
+                LoginIntent.putExtra("cek_login",true);
+                startActivity(LoginIntent);
+            }else if(password.isEmpty()){
+                Toast.makeText(this,"Yah Gagal",Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
+//    private void cekCrededentials(){
+//        String username = editUsername.getEditableText().toString();
+//        String password = editPassword.getEditableText().toString();
+//
+//        if(password.equals("yeyjadi")){
+//            Intent LoginIntent = new Intent(this, MainMenu.class);
+//            LoginIntent.putExtra("username",username);
+//            LoginIntent.putExtra("cek_login",true);
+//            startActivity(LoginIntent);
+//        }else if(password.isEmpty()){
+//            Toast.makeText(this,"Yah Gagal",Toast.LENGTH_SHORT).show();
+//        }
+//    }
+//    public void openMainMenu(){
+//        Intent LoginIntent = new Intent(this, MainMenu.class);
+//        startActivity(LoginIntent);
+//        finish();
+//    }
 }
