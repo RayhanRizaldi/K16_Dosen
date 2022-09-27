b2109ca4ac16aff456c431c6ade0f1befd047894
RayhanRizaldi <2011522021@student.unand.ac.id>
2022-09-27 11:24:28 +0700

    rycycleview

--- app/src/main/java/com/tbptb_b/k16_dosen/MainMenu.java
+++ app/src/main/java/com/tbptb_b/k16_dosen/MainMenu.java
@@ -1,6 +1,8 @@
 package com.tbptb_b.k16_dosen;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,22 +12,26 @@ import android.view.View;
 import android.widget.Button;
 
 import android.widget.ImageButton;
+import com.tbptb_b.k16_dosen.models.ListNamaMahasiswa;
 
+import java.util.ArrayList;
 
 public class MainMenu extends AppCompatActivity {
 Button buttondetailta;
 //    private Boolean isLoggedIn = false;
-    ImageButton dpbtt;
+    private RecyclerView rvListmaba;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
-//        if(isLoggedIn == false){
-//            Intent loginintent = new Intent(this, LoginActivity.class);
-//            startActivity(loginintent);
-//        }
+        rvListmaba = findViewById(R.id.rv_listmhs);
 
+        Adapter adapter = new Adapter(getListNamaMahasiswa());
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+
+        rvListmaba.setLayoutManager(layoutManager);
+        rvListmaba.setAdapter(adapter);
 
         buttondetailta = findViewById(R.id.more_bimbingan);
         buttondetailta.setOnClickListener(new View.OnClickListener() {
@@ -34,6 +40,47 @@ Button buttondetailta;
                 openDetailta();
             }
         });
+//        if(isLoggedIn == false){
+//            Intent loginintent = new Intent(this, LoginActivity.class);
+//            startActivity(loginintent);
+//        }
+
+//        dpbtt = findViewById(R.id.imageButtonListDetailMabim);
+//        dpbtt.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent detailIn = new Intent(MainMenu.this, DetailMahasiswa .class);
+//                startActivity(detailIn);
+//            }
+//        });
+
+    }
+
+    public ArrayList<ListNamaMahasiswa> getListNamaMahasiswa(){
+        ArrayList<ListNamaMahasiswa> listnamamhs = new ArrayList<>();
+        listnamamhs.add(new ListNamaMahasiswa(
+                null,
+                "Budy Bahahahaha",
+                2011623912
+        ));
+        listnamamhs.add(new ListNamaMahasiswa(
+                null,
+                "Alfred Alfonso",
+                98303928
+        ));
+        listnamamhs.add(new ListNamaMahasiswa(
+                null,
+                "Xavier the Third",
+                33333333
+        ));
+        listnamamhs.add(new ListNamaMahasiswa(
+                null,
+                "Nadila Saraswati",
+                2011521004
+        ));
+        return listnamamhs;
+
+
     }
 
     public void openDetailta(){
