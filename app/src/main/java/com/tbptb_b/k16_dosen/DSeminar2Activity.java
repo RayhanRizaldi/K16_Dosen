dd5bcca38be73b36b5bfa12a490228e7218be7c1
ZuhaBima <zuhabima@gmail.com>
2022-09-30 17:22:21 +0700

    bismillah

---
+++ app/src/main/java/com/tbptb_b/k16_dosen/DSeminar2Activity.java
@@ -0,0 +1,15 @@
+package com.tbptb_b.k16_dosen;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DSeminar2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dseminar2);
+
+    }
+}
