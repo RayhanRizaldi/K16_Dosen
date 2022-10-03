2985e6c7ef5f89d5b431de8fb293a9133b5a90fc
ZuhaBima <zuhabima@gmail.com>
2022-10-03 19:43:38 +0700

    bismillah

---
+++ app/src/main/java/com/tbptb_b/k16_dosen/formpembatalanTAActivity.java
@@ -0,0 +1,15 @@
+package com.tbptb_b.k16_dosen;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class formpembatalanTAActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.form_pembatalan_ta);
+
+    }
+}
