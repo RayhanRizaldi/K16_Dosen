34441cdd9b4dcc7710affe64b80d2c900a281c08
winanda afrilia harisya <winandaafrilia0304@gmail.com>
2022-10-07 00:52:28 +0700

    List RV selesai

---
+++ app/src/main/java/com/tbptb_b/k16_dosen/JsidangActivity.java
@@ -0,0 +1,14 @@
+package com.tbptb_b.k16_dosen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JsidangActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jsidang);
+    }
+}
