24da0d8a167d785f0146886d36a86c13fbc43a32
ZuhaBima <zuhabima@gmail.com>
2022-10-13 17:18:26 +0700

    bisha

--- app/src/main/java/com/tbptb_b/k16_dosen/RekomendasiInputActivity.java
+++ app/src/main/java/com/tbptb_b/k16_dosen/RekomendasiInputActivity.java
@@ -1,14 +1,14 @@
 package com.tbptb_b.k16_dosen;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-public class inputrekomendasiActivity extends AppCompatActivity {
+import android.os.Bundle;
+
+public class RekomendasiInputActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_inputrekomendasi);
+        setContentView(R.layout.activity_rekomendasi_input);
     }
 }
---
+++ app/src/main/java/com/tbptb_b/k16_dosen/SeminarPersetujuanActivity.java
@@ -0,0 +1,70 @@
+package com.tbptb_b.k16_dosen;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.tbptb_b.k16_dosen.adapter.jsid_adapter;
+import com.tbptb_b.k16_dosen.models.jsid_model;
+
+import java.util.ArrayList;
+
+public class SeminarPersetujuanActivity extends AppCompatActivity  implements jsid_adapter.ItemjsidClickListener {
+    private RecyclerView rvssem;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seminar_persetujuan);
+        rvssem = findViewById(R.id.rv_persetujuansem);
+
+        jsid_adapter jsidadapter = new jsid_adapter(getpersetujuanseminar_model());
+        jsidadapter.setListener(this);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+
+        rvssem.setLayoutManager(layoutManager);
+        rvssem.setAdapter(jsidadapter);
+
+
+    }
+
+    public ArrayList<jsid_model> getpersetujuanseminar_model() {
+        ArrayList<jsid_model> listjsid_model = new ArrayList<>();
+
+        listjsid_model.add(new jsid_model(
+                null,
+                "selasa, 11 oktober 2022",
+                "Winanda afrilia harisya",
+                "2011522016"
+
+        ));
+
+        listjsid_model.add(new jsid_model(
+                null,
+                "rabu, 12 oktober 2022",
+                "Nadilla saraswati",
+                "2011523012"
+
+        ));
+
+        listjsid_model.add(new jsid_model(
+                null,
+                "kamis, 13 oktober 2022",
+                "Yupiko",
+                "2011521001"
+
+        ));
+
+        return listjsid_model;
+    }
+
+    @Override
+    public void onitemjsidClick(jsid_model jsidmodel) {
+        Intent dsemIntent = new Intent(this, DSeminar2Activity.class);
+
+        startActivity(dsemIntent);
+    }
+}
