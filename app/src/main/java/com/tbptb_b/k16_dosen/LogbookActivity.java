6c4dedac87acdc76de6ef35e30f7cbc3039120d8
NadillaSaraswati <90832058+NadillaSaraswati@users.noreply.github.com>
2022-12-08 01:39:40 +0700

    notif lb

--- app/src/main/java/com/tbptb_b/k16_dosen/LogbookActivity.java
+++ app/src/main/java/com/tbptb_b/k16_dosen/LogbookActivity.java
@@ -1,13 +1,23 @@
 package com.tbptb_b.k16_dosen;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.tbptb_b.k16_dosen.adapter.listlogbook_adapter;
 import com.tbptb_b.k16_dosen.models.Listlogbook;
@@ -17,7 +27,10 @@ import java.util.ArrayList;
 public class LogbookActivity extends AppCompatActivity implements listlogbook_adapter.ListLogbookClickListener{
 Button buttondetaillb;
 
+private static final String CHANNEL_ID = "test_kanal";
+private Button buttonnotif;
 private RecyclerView rvLogbook;
+private NotificationManagerCompat notificationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +46,34 @@ private RecyclerView rvLogbook;
         rvLogbook.setLayoutManager(layoutManager);
         rvLogbook.setAdapter(adapter);
 
+        notificationManager = NotificationManagerCompat.from(this);
+
+        createNotificationChannel();
+
+        buttonnotif = findViewById(R.id.notiflb);
+        buttonnotif.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent resultIntent = new Intent(LogbookActivity.this, LogbookActivity.class);
+
+                TaskStackBuilder stackBuilder = TaskStackBuilder.create(LogbookActivity.this);
+                stackBuilder.addNextIntentWithParentStack(resultIntent);
+                PendingIntent resultPendingIntent =
+                        stackBuilder.getPendingIntent(0,
+                                PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(LogbookActivity.this, CHANNEL_ID)
+                        .setSmallIcon(R.drawable.app_logo)
+                        .setContentTitle("Ada Logbook")
+                        .setContentText("Ada Logbook terbaru, ayo lihat!!")
+                        .setContentIntent(resultPendingIntent)
+                        .addAction(R.drawable.app_logo, "CEK", resultPendingIntent)
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+                Notification notification = builder.build();
+                notificationManager.notify(101,notification);
+            }
+        });
+
 //        buttondetaillb = findViewById(R.id.buttondetaillogbook);
 //        buttondetaillb.setOnClickListener(new View.OnClickListener() {
 //            @Override
@@ -41,6 +82,18 @@ private RecyclerView rvLogbook;
 //            }
 //        });
     }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "notifikasi logbook";
+            String description = "Ada Logbook masuk";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "jadwal Sidang", NotificationManager.IMPORTANCE_HIGH);
+            channel.setDescription("notifikasi jadwal sidang");
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
      public ArrayList<Listlogbook> getLislogbook(){
         ArrayList<Listlogbook> ListLogbook = new ArrayList<>();
         ListLogbook.add(new Listlogbook(
